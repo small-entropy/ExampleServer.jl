a1b660743bdb5927e1234a445e306467f7b9ffd3
Dmitrii Shevelev <igrave1988@gmail.com>
2020-09-16 17:42:56 +0500

    Добавил немного кода

--- src/ExampleServer.jl
+++ src/ExampleServer.jl
@@ -1,5 +1,15 @@
 module ExampleServer
+    using Immortal
 
-# Write your package code here.
+    include("./Handlers.jl")
+    import .Handlers
 
+
+    function run()
+        config_path::String = "/home/entropy/Проекты/ExampleServer/config.yml"
+
+        connection, broker_config, channels_config = Immortal.init(config_path)
+        channels_dicts = Immortal.declares(connection, broker_config, channels_config)
+        Adapter = Immortal.get_adapter(broker_config)
+    end
 end
---
+++ src/Handlers.jl
@@ -0,0 +1,7 @@
+module Handlers
+    export test_handler
+
+    function test_handler(Adapter, channel, message)
+        print("Тестовый обработчик")
+    end
+end
